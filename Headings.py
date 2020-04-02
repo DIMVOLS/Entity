d0678f1a6b1ba5c239f2be365355ca596ebec8e2
DmitriyDM <61846674+DIMVOLS@users.noreply.github.com>
2020-04-03 00:16:05 +0300

    Update Headings.py

--- Headings.py
+++ Headings.py
@@ -28,9 +28,9 @@ from statistics import mode,mean
 #doc_id='edbe20b8959e4f1bb0e68b4cb4a52c5b'    #25.pdf
 #doc_id='f5d11cf0148e45c3b6b2b40266ac179d'     #16.pdf
 #doc_id='3f8ae2df94824b3382305e9474ab40b1'      #10.pdf
-#doc_id='1840379e455a4d09899d0ff69235effe'    #10-1.pdf
+doc_id='1840379e455a4d09899d0ff69235effe'    #10-1.pdf
 #doc_id='6773c526e29e402c8077331a7ae59348'    #10-2.pdf
-doc_id='7cf28796e51a491397ad7886e3c96b78'     #10-3.pdf
+#doc_id='7cf28796e51a491397ad7886e3c96b78'     #10-3.pdf
 #doc_id='e9c24931ffa84a7d801851eb433a3779'    #10-4.pdf
 #doc_id='a89140e96b134499b8a9dee111cda140'    #10-5.pdf
 #doc_id='1f55083d0a204bcdbfb8e4ee924dc2c1'    #10-6.pdf
@@ -42,49 +42,43 @@ with open('tmp/local_storage/{}.dm'.format(doc_id), 'rb') as mf:
     doc = pickle.load(mf)
 page = doc.pages[0]
 
-#print(page.blocks)
+
 print("------------------------------------------------------------------------------------------------------------")
-#print(page.bbox)
-blockss=[]
+
 b_h_loc=[]
 i=0
-up_cor=[]
+confidence_score=[]
 case=[]
 size=[]
-size_case=[]
-max_size=0
 b_w_amount=[]
-max_words=0
-max_height=0
-max_center=0
-block_center=[]
+block_empt=[]
+block_center2=[]
 Texts=[]
 prev_bot=0
 b_margin=[]
-w_height=[]
 symbol_w=[]
 tags=[]
-size2=[]
-size3=[]
+spec_symbols=[]
 p_Left,p_Top,p_Right,p_Bottom=page.bbox[0],page.bbox[1],page.bbox[2],page.bbox[3]
 #print(p_Left,p_Top,p_Right,p_Bottom)
+results=[]
 for block in page.blocks:
-   # print("BLOCK #",i)
     w_width=[]
     count=0
     lines_height=0
     uc_words = 0
     t_words = 0
     b_Left,b_Top,b_Right,b_Bottom=block.bbox[0],block.bbox[1],block.bbox[2],block.bbox[3]
-   # print("COORDINATES",b_Left,b_Top,b_Right,b_Bottom)
-    block_center.append((b_Left+(p_Right-b_Right))/2)
-    #print("CENTER: ",block_center[i],b_Left,p_Right/2)
+    block_empt.append((b_Left+(p_Right-b_Right))/2)
+    block_center2.append(b_Left/(p_Right/2))
+    
+    #print(block_center2)
+    #print()
     
     block_words = " ".join([word.value for line in block.lines for word in line.words])
     text = nltk.word_tokenize(block_words)
     pos_tagged = nltk.pos_tag(text)
-    #print(block_words) 
-    #print(pos_tagged)
+
     nouns=list(filter(lambda x:x[1] in ('NN','NNS','NNP','NNPS'),pos_tagged))
     #print(nouns)
     verbs=list(filter(lambda x:x[1] in ('VB','VBD','VBZ','VBG','VBP','VBN','MD'),pos_tagged))
@@ -94,14 +88,13 @@ for block in page.blocks:
     if(len(nouns)>0):
         count=count+1
     
-        #print("Nouns prob")
     if(len(verbs)>1):
         count=count-1
-        #print("Verbs prob")
         
     if(len(digits)>0):
         count=count-1
-        #print("Digits prob")
+    if any([True for x in ['link:','http:','https:','www.','.com','.html'] if x in block_words]):
+        count=count-1
         
     if count<1:
         tags.append(0)
@@ -111,46 +104,45 @@ for block in page.blocks:
     Texts.append(block_words)
     #print(block_words)
     b_w_amount.append(len(block_words.split()))
-    #print(block_words,block_w_amount)
-   # print("Words AMOUNT: ",block_w_amount)
+
     b_margin.append(b_Top-prev_bot)
     prev_bot=b_Bottom
 
-    if len(block_words.split())>=max_words:
-        max_words=len(block_words.split())    
-        
     b_h_loc.append(b_Bottom/p_Bottom)
-    #print("BLOCK_HEIGHT:",b_h_loc)
-    if b_h_loc[i]>=max_height:
-        max_height=b_h_loc[i]
-        
-    lines_height=[]    
+      
+    lines_height=[]
+
     for line in block.lines:
         Left,Top,Right,Bottom=line.bbox[0],line.bbox[1],line.bbox[2],line.bbox[3]
         lines_height.append(Bottom-Top)
+        line_words = " ".join([word.value for word in line.words])
+       # print(line_words)
         for word in line.words:
             w_width.append(word.get_symbol_sizes()[0][0])
             w_height.append(word.get_symbol_sizes()[0][1])
             if word.value.isupper():
                 uc_words = uc_words + 1
             elif word.value.istitle():
-                t_words = t_words + 1 
-        size3.append(max(w_height))        
-    #print("WORDS SIZE",w_width)
+                t_words = t_words + 1           
+                
     symbol_w.append(mean(w_width))
-    size2.append(mean(size3))
-    #print(size2)
-    #print("SIZE:",i,lines_height)            
-    size.append(mode(lines_height))
-    if size[i]>=max_size:
-        max_size=size[i]   
+    
+    
+    if any([True for x in [';',',','//',':','<','>','*'] if x in block_words]) or block_words.endswith('.'):
+        spec_symbols.append(0)
+    else:
+        spec_symbols.append(1)
+    
+                
+    size.append(mean(lines_height))
         
     if b_Left>p_Right/2:
-        block_center[i]=0
-        size[i]=0
-        symbol_w[i]=0
+        block_empt[i]=0
+        block_center2[i]=0
+    #print(block_empt)    
         
-    #print("Amount lines and size:",len(block.lines),size)
+       
+ 
     upercent=uc_words/b_w_amount[i]
     tpercent=t_words/b_w_amount[i]
     #print("Upper and Title case percents: ",upercent,tpercent)
@@ -164,69 +156,81 @@ b_margin.append(p_Bottom-b_Bottom)
 
 b_w_amount=([1-x/mean(b_w_amount) if x<mean(b_w_amount) else 0 for x in b_w_amount])
 
-print(b_w_amount)
-
 b_h_loc=([1-(x-min(b_h_loc))/(max(b_h_loc)-min(b_h_loc)) for x in b_h_loc])
 aver_size=mode(size)
-#print(tags)
-#print(aver_size)
-size2=[x for x in size2] 
-size2=[(x-min(size2))/(max(size2)-min(size2)) for x in size2] 
-size=[x/mean(size) for x in size] 
+
 size=[(x-min(size))/(max(size)-min(size)) for x in size]    
 symbol_w=[(x-min(symbol_w))/(max(symbol_w)-min(symbol_w)) for x in symbol_w]
-#print("W",symbol_w)
-#print("C",case)
-#print(size)
-block_center=[(x-min(block_center))/(max(block_center)-min(block_center)) for x in block_center]
+
+block_empt=[(x-min(block_empt))/(max(block_empt)-min(block_empt)) for x in block_empt]
+block_center2=[(x-min(block_center2))/(max(block_center2)-min(block_center2)) for x in block_center2]
+center_empt=[(x+y)/2 for x,y in zip(block_empt,block_center2)]
+center_empt=[(x-min(center_empt))/(max(center_empt)-min(center_empt)) for x in center_empt]
+
 case=[x if x>0.5 else 0 for x in case]
 if any([True for x in case if x > 0.5]):
     case=[(x-min(case))/(max(case)-min(case)) for x in case]
 else:
     case=[0]*len(case)
-#case=[x if x>0.5 else 0 for x in case]
-#print(case)
-#w_case=[(y*z) for y,z in zip(case,b_w_amount)]
-#print("size+case2: ",size_case)
-Features=[x*0.4+a*0.5+z*0+y*0.25+c*0.25+b*0.25+v*0.12 for x,a,z,y,c,b,v in 
-          zip(size,symbol_w,b_h_loc,block_center,case,b_w_amount,tags)]
-b_h_loc=[x if y>0.7 else 0  for x,y in zip(b_h_loc,Features)]
+
+Features=[x*0.35+a*0.45+y*0.2+c*0.22+b*0.33 for x,a,y,c,b in 
+          zip(size,symbol_w,block_empt,case,b_w_amount)]
+b_h_loc=[x if y>0.75 else 0  for x,y in zip(b_h_loc,Features)]
 Features=[x+y*0.3 for x,y in zip(Features,b_h_loc)]
 #Features=[x*0.8+(z*y)*0.3+c*0.1+b*0.2 for x,y,z,c,b in zip(size,block_height_loc,block_center,case,b_w_amount)]
 Final=dict(zip(Features,Texts))
 m=max(Final)
-if any([True for x in Features if x>1]):
-    print("Headings Found")
+
+f_threshold=[1 if x>1 else 0 for x in Features]
+print("Spec   ",spec_symbols)
+print("Tags   ",tags)
+print("Thresh ",f_threshold)
+confidence_score=[(x+y+z)/3 for x,y,z in zip(spec_symbols,tags,f_threshold)]
+
+#norm_Features=[(x/max(Features))for x in Features]
+norm_Features=[(x-min(Features))/(max(Features)-min(Features)) if y > 0.5 
+               else (x-min(Features))/(max(Features)-min(Features))*0.1 
+               for x,y in zip(Features,confidence_score)]
+norm_Final=dict(zip(norm_Features,Texts))
+
+if any([True for x in norm_Features if x>0.5]):
+    print("Headings Found:",len([x for x in norm_Features if x>0.5]))
+    
+    print("MAIN HEADING - ",norm_Final[max(norm_Final)])
+    print("----------------------------------------------------------------------------------------------------------")
+    for key in norm_Final:
+        if key < max(norm_Final) and key > 0.5:
+            print("SUB HEADINGS - ",norm_Final[key])
+    print("----------------------------------------------------------------------------------------------------------")        
+
 else:
     print("Headings Not Found")
-    
-norm_Features=[(x/max(Features))for x in Features]
-norm_Features2=[(x-min(Features))/(max(Features)-min(Features))for x in Features]
-norm_Final=dict(zip(norm_Features,Texts))
-print("MAIN HEADING - ",norm_Final[max(norm_Final)])
-print("----------------------------------------------------------------------------------------------------------")
+#print(norm_Features)   
+#print(norm_Final)
+i=0
 for key in norm_Final:
-    if key < max(norm_Final) and key > 0.5:
-        print("SUB HEADINGS - ",norm_Final[key])
-print("----------------------------------------------------------------------------------------------------------")        
-for key in norm_Final:
-    print(key,norm_Final[key])       
-plt.figure()
-plt.ylabel("SIZE")
-plt.xlabel("Block")      
-plt.plot(np.arange(0,len(page.blocks)),size2,label="size2")
-plt.legend()    
+    print("#{} - {:.2f}, {}".format(i,key,norm_Final[key]))
+    if key>0.5:
+        results.append(i)
+    i=i+1    
+#with open('/home/dim/results.txt', 'a') as the_file:
+#    the_file.write(doc_id+'\n'+str(results)+'\n')
+print(results)        
 plt.figure()
 plt.ylabel("SIZE")
 plt.xlabel("Block")      
 plt.plot(np.arange(0,len(page.blocks)),size,label="size")
+plt.plot(np.arange(0,len(page.blocks)),symbol_w,label="width")
 plt.legend()
 plt.figure()
-plt.ylabel("Center")
-plt.xlabel("Block")  
-plt.plot(np.arange(0,len(page.blocks)),block_center,label="center")
+plt.ylabel("Center and empty")
+plt.xlabel("Block")
+plt.plot(np.arange(0,len(page.blocks)),center_empt,label="center_empt")
+plt.plot(np.arange(0,len(page.blocks)),block_empt,label="empty")
+plt.plot(np.arange(0,len(page.blocks)),block_center2,label="center2") 
 plt.legend()
-plt.ylabel("Height_location")
+plt.figure()
+plt.ylabel("Center and h_location")
 plt.xlabel("Block")  
 plt.plot(np.arange(0,len(page.blocks)),b_h_loc,label="height_location")
 plt.legend()
@@ -240,25 +244,30 @@ plt.xlabel("Block")
 plt.plot(np.arange(0,len(page.blocks)),b_w_amount,label="b_w_amount")
 plt.legend()
 plt.figure()
-plt.ylabel("Symbols width")
+plt.ylabel("Features")
 plt.xlabel("Block")  
-plt.plot(np.arange(0,len(page.blocks)),symbol_w,label="width")
+plt.plot(np.arange(0,len(page.blocks)),tags,label="tags")
+plt.plot(np.arange(0,len(page.blocks)),spec_symbols,label="spec_symbols")
 plt.legend()
 plt.figure()
 plt.ylabel("Features")
 plt.xlabel("Block")  
-plt.plot(np.arange(0,len(page.blocks)),tags,label="tags")
+plt.ylim(0, 2)
+plt.plot(np.arange(0,len(page.blocks)),Features,label="Final")
+plt.plot(np.arange(0,len(page.blocks)),[1] * len(page.blocks) )
+plt.legend()
+plt.figure()
+plt.ylabel("Features")
+plt.xlabel("Block")  
+plt.ylim(0, 1.01)
+plt.plot(np.arange(0,len(page.blocks)),confidence_score,label="Confidence_score")
+plt.plot(np.arange(0,len(page.blocks)),[0.5] * len(page.blocks) )
 plt.legend()
 plt.figure()
 plt.ylabel("norm_Features")
 plt.xlabel("Block")  
+#plt.ylim(0, 1.01)
 plt.plot(np.arange(0,len(page.blocks)),norm_Features,label="norm_Final")
-plt.plot(np.arange(0,len(page.blocks)),[0.5] * len(page.blocks) )
-plt.legend()
-plt.figure()
-plt.ylabel("Features")
-plt.xlabel("Block")  
-plt.plot(np.arange(0,len(page.blocks)),Features,label="Final")
-plt.plot(np.arange(0,len(page.blocks)),[0.5] * len(page.blocks) )
+#plt.plot(np.arange(0,len(page.blocks)),[0.5] * len(page.blocks) )
 plt.legend()
 plt.show()      
